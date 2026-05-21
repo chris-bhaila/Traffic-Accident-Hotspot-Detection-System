416bc8726df8f434254e257e10499bfe99aae1bf
chris-bhaila <chrisbhaila5@gmail.com>
2026-05-21 16:30:25 +0545

    Fix sys.path in save_clusters and save_tree

--- scripts/save_clusters.py
+++ scripts/save_clusters.py
@@ -1,6 +1,8 @@
 import os
 import django
+import sys
 
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django.setup()
 

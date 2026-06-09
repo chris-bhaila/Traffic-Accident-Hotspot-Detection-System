201d2c33f1636594d75af063ce1da0852876baf1
chris-bhaila <chrisbhaila5@gmail.com>
2026-06-09 21:20:44 +0545

    Remove debug endpoint

--- predictions/urls.py
+++ predictions/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
-from predictions.views import debug_centroids
 
 urlpatterns = [
     path("predict/", views.predict_risk, name="predict_risk"),
     path("route-risk/", views.route_risk, name="route_risk"),
-    path('debug/', views.debug_centroids),
 ]
